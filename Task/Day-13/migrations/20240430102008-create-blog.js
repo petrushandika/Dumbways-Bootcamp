4999aca59e7572cf7a8ab218d98cfba23f0764b6
petrushandika <petrushandikasinaga@gmail.com>
2024-05-02 21:15:38 +0700

    Data Modeling

---
+++ Task/Day-13/migrations/20240430102008-create-blog.js
@@ -0,0 +1,49 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('blogs', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      start_date: {
+        type: Sequelize.DATE
+      },
+      end_date: {
+        type: Sequelize.DATE
+      },
+      node: {
+        type: Sequelize.BOOLEAN
+      },
+      react: {
+        type: Sequelize.BOOLEAN
+      },
+      angular: {
+        type: Sequelize.BOOLEAN
+      },
+      golang: {
+        type: Sequelize.BOOLEAN
+      },
+      image: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('blogs');
+  }
+};
